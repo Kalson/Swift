9e9d3d8d8f18ff7a00f8fa4bcd0867f8bb29ca54
KaL <Kalson.Kalu@yahoo.com>
2014-08-26 13:50:26 -0400

    Swiftly Parse - fix compiler error
    sourcekitservice terminated

---
+++ Swiftly Parse/Swiftly Parse/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Swiftly Parse
+//
+//  Created by KaL on 8/26/14.
+//  Copyright (c) 2014 Kalson Kalu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+
+    func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Swiftly Parse/Swiftly Parse/FaceCell.swift
@@ -0,0 +1,26 @@
+//
+//  FaceCell.swift
+//  Swiftly Parse
+//
+//  Created by KaL on 8/26/14.
+//  Copyright (c) 2014 Kalson Kalu. All rights reserved.
+//
+
+import UIKit
+
+class FaceCell: UITableViewCell {
+
+    @IBOutlet weak var faceView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
