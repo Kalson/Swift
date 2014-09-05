684bfcae7c586bb52f4f543741aaad94475e68ca
KaL <Kalson.Kalu@yahoo.com>
2014-09-05 10:09:33 -0400

    robot setuping up

---
+++ My Lil Robot/My Lil Robot/RobotPart.swift
@@ -0,0 +1,21 @@
+//
+//  RobotPart.swift
+//  My Lil Robot
+//
+//  Created by KaL on 9/5/14.
+//  Copyright (c) 2014 KaL. All rights reserved.
+//
+
+import UIKit
+
+class RobotPart: UIView {
+   
+    func addPartToRobotAtPoint(robot: UIView, point: CGPoint){
+        // can't add a subview to NSObject
+        
+        // everything is a subview
+        
+//        robot.addSubview(<#view: UIView#>)
+        
+    }
+}
