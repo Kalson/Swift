0159bf1b7291b44d813127d9393c7d31c9229532
KaL <Kalson.Kalu@yahoo.com>
2014-09-09 11:35:51 -0400

    update for sections and more

--- 🙈🙉🙊/🙈🙉🙊/😎TableViewController.swift
+++ 🙈🙉🙊/🙈🙉🙊/😎TableViewController.swift
@@ -12,17 +12,34 @@ class 😎TableViewController: UITableViewController {
     
     
 //    var letters: [String]!
-    var users:[String:[String]]!
+    var users:[String:[String]] = [
+        "A":["Arthur"],
+        "B":["Bitcoin"],
+        "D":["Daniel"],
+        "E":["Eric S","Eric W"],
+        "H":["Heidi"],
+        "J":["Jaime","Jeremy","Jo","Josh"],
+        "K":["Kalson","Katyln"],
+        "M":["Meritt"],
+        "N":["Nick"],
+        "R":["Rene"],
+        "S":["Shane","Steve"],
+        
+
+    ]
     // ! sets it to be an empty dictionary
     // dictionary for the key, we want an array of keys
     
 //    var d1: [String:String]!
 //    var d2: NSDictionary!
 
+    var letters:[String]!
+    // array of strings being declared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        letters = users.keys.array.sorted(<)
         
 
         // Uncomment the following line to preserve selection between presentations
@@ -48,6 +65,8 @@ class 😎TableViewController: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
+        println(users.keys.array)
+        
         return users.keys.array.count
         // refers to the keys of arrays
     }
@@ -56,7 +75,7 @@ class 😎TableViewController: UITableViewController {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
         
-        var key = users.keys.array[section]
+        var key = users.keys.array.sorted(<)[section]
         var usersInSection = users[key] as Array?
         // return the value based on the key (its returning the array of strings in this case)
         
@@ -74,17 +93,34 @@ class 😎TableViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("userCell", forIndexPath: indexPath) as UITableViewCell
 
-        var key = users.keys.array[indexPath.section]
+        var key = letters[indexPath.section]
         var usersInSection = users[key]
         
         cell.textLabel?.text = usersInSection![indexPath.row]
 
-
+        println(usersInSection![indexPath.row] + "row:\(indexPath.row) section:\(indexPath.section)")
+        
         // Configure the cell...
 
         return cell
     }
 
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        var headerView = UIView(frame: CGRectMake(0, 0, self.tableView.frame.size.width, 20))
+        
+        headerView.backgroundColor = UIColor(white: 0.0, alpha: 0.3)
+        
+        var label = UILabel(frame: CGRectMake(15, 0, 200, 20))
+        label.text = letters[section]
+        label.textColor = UIColor.whiteColor()
+        // the label is put inside the UIView | text can't be written in a UIView, thats why we use a label
+        headerView.addSubview(label)
+        
+        return headerView
+    }
+    
+    // every method that a class has that u can called is overiding
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView!, canEditRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
