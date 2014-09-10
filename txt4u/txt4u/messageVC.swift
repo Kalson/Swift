26df869b807d35a8a9c521079d8df421c6353db8
KaL <Kalson.Kalu@yahoo.com>
2014-09-10 16:07:27 -0400

    update

--- txt4u/txt4u/messageVC.swift
+++ txt4u/txt4u/messageVC.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class messageVC: UIViewController {
+class messageVC: UIViewController, UITableViewDelegate,UITableViewDataSource {
     @IBOutlet weak var formHolder: UIView!
     @IBOutlet weak var messageField: UITextField!
     @IBOutlet weak var sendButton: UIButton!
-
-    var friend: PFUser!
     
-    var conservation: [PFObject]!
+    @IBOutlet weak var tableView: UITableView!
+    // were not gonna use those the prototype cell for this tableview
+
+    var friend: PFUser! {
+        
+        willSet(user) {
+            println("inside will set \(user)")
+        }
+        
+        didSet(user){
+            println("inside did set \(user)")
+        }
+    }
+    
+    var conservation: [PFObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return conservation.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("messageCell", forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = conservation[indexPath.row]["content"] as String?
+        return cell
     }
     
     
@@ -29,18 +55,38 @@ class messageVC: UIViewController {
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
-        conservation = defaults.arrayForKey(friend.username) as [PFObject]
+        println("inside will appear\(friend)")
+        
+        
+//        conservation = defaults.arrayForKey(friend.username) as [PFObject]!
         
         var messageQuery = PFQuery(className: "Message")
         messageQuery.whereKey("sender", equalTo: PFUser.currentUser())
         
         messageQuery.findObjectsInBackgroundWithBlock { (messages: [AnyObject]!,error: NSError!) -> Void in
-            self.conservation = messages as [PFObject]
+            
+            if messages.count > 0 {
+                self.conservation = messages as [PFObject]
+                self.tableView.reloadData()
+            }
         }
     }
 
     @IBAction func sendMessage(sender: AnyObject) {
         
+        // PFobject is built like a dictionary so it has keys and values
+        
+        var message = PFObject(className: "Message")
+        message["sender"] = PFUser.currentUser().username
+        message["receiver"] = friend
+        message["content"] = messageField.text
+        
+        conservation.append(message)
+        
+        message.saveInBackground()
+        tableView.reloadData()
+
+    
     }
  
 
