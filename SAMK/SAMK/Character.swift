2150142c96a9c663eb611903d304023895c8122f
KaL <Kalson.Kalu@yahoo.com>
2014-09-22 10:26:17 -0400

    initial setup

---
+++ SAMK/SAMK/Character.swift
@@ -0,0 +1,13 @@
+//
+//  Character.swift
+//  SAMK
+//
+//  Created by KaL on 9/22/14.
+//  Copyright (c) 2014 Kalson Kalu. All rights reserved.
+//
+
+import UIKit
+
+class Character: NSObject {
+   
+}
---
+++ SAMK/SAMK/PlayerConnect.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerConnect.swift
+//  SAMK
+//
+//  Created by KaL on 9/22/14.
+//  Copyright (c) 2014 Kalson Kalu. All rights reserved.
+//
+
+import UIKit
+
+class PlayerConnect: NSObject {
+   
+}
